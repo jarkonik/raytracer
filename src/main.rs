5057c558df1f9b36de8b43f527d5ecd18e76af81
Jaroslaw Konik <konikjar@gmail.com>
2019-08-18 14:05:19 +0200

    Works

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,56 @@
 extern crate image;
 
 mod vector;
-use vector::Vector;
+use vector::{collide, Vector};
 
 const EYE: Vector = Vector {
     x: 0.0,
     y: 0.0,
-    z: 0.0,
+    z: 10.0,
 };
-const SPHERE: Vector = Vector {
-    x: 0.0,
-    y: 0.0,
-    z: -60.0,
-};
-const SPHERE_RADIUS: f64 = 50.0;
-const VIEW_PLANE_DIST: f64 = 100.0;
+
+#[derive(Copy, Clone, Debug)]
+struct Sphere {
+    center: Vector,
+    radius: f64,
+    color: [u8; 3],
+}
+
+const VIEW_PLANE_DIST: f64 = 400.0;
 const IMG_WIDTH: u32 = 800;
 const IMG_HEIGHT: u32 = 800;
+const LIGHT_POINT: Vector = Vector {
+    x: 0.0,
+    y: 0.0,
+    z: 0.0,
+};
 
-fn intersects(origin: Vector, dir: Vector, center: Vector, r: f64) -> bool {
-    let l = center - origin;
-    let tc = l.dot(dir);
+const SPHERES: [Sphere; 2] = [
+    Sphere {
+        center: Vector {
+            x: 0.0,
+            y: 0.0,
+            z: -150.0,
+        },
+        radius: 50.0,
+        color: [255, 0, 0],
+    },
+    Sphere {
+        center: Vector {
+            x: 50.0,
+            y: 0.0,
+            z: -60.0,
+        },
+        radius: 50.0,
+        color: [0, 255, 0],
+    },
+];
 
-    if tc < 0.0 {
-        return false;
-    }
-
-    let r2 = r * r;
-
-    let d2 = l.dot(l) - tc * tc;
-
-    return d2 <= r2;
+#[derive(Copy, Clone, Debug)]
+struct Collision {
+    distance: f64,
+    sphere: Sphere,
+    hit_point: Vector,
 }
 
 fn main() {
@@ -44,13 +64,50 @@ fn main() {
         }
         .normalize();
 
-        let (r, g, b) = if intersects(EYE, ray_dir, SPHERE, SPHERE_RADIUS) {
-            (255, 0, 0)
-        } else {
-            (255, 255, 255)
-        };
+        let collisions: Vec<Collision> = SPHERES
+            .iter()
+            .filter_map(|&sphere| -> Option<Collision> {
+                let hit_point = collide(EYE, ray_dir, sphere.center, sphere.radius);
+                match hit_point {
+                    Some(point) => Some(Collision {
+                        distance: (point - EYE).magnitude(),
+                        hit_point: point,
+                        sphere: sphere,
+                    }),
+                    None => None,
+                }
+            })
+            .collect();
 
-        *pixel = image::Rgb([r, g, b]);
+        if collisions.is_empty() {
+            continue;
+        }
+
+        let closest_collision = collisions.iter().fold(collisions[0], |acc, &collision| {
+            if collision.distance < acc.distance {
+                collision
+            } else {
+                acc
+            }
+        });
+
+        let in_shadow = SPHERES.iter().any(|sphere| {
+            collide(
+                closest_collision.hit_point,
+                (LIGHT_POINT - closest_collision.hit_point).normalize(),
+                sphere.center,
+                sphere.radius,
+            )
+            .is_some()
+        });
+
+        if in_shadow {
+            continue;
+        }
+
+        let color = closest_collision.sphere.color;
+
+        *pixel = image::Rgb(color);
     }
 
     imgbuf.save("test.png").unwrap();
--- src/vector.rs
+++ src/vector.rs
@@ -21,6 +21,18 @@ impl Vector {
     }
 }
 
+impl ops::Neg for Vector {
+    type Output = Vector;
+
+    fn neg(self) -> Vector {
+        Vector {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
 impl ops::Add<Vector> for Vector {
     type Output = Vector;
 
@@ -29,6 +41,18 @@ impl ops::Add<Vector> for Vector {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
             z: self.z + rhs.z,
+        }
+    }
+}
+
+impl ops::Mul<f64> for Vector {
+    type Output = Vector;
+
+    fn mul(self, rhs: f64) -> Vector {
+        Vector {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
         }
     }
 }
@@ -55,4 +79,34 @@ impl ops::Sub for Vector {
             z: self.z - rhs.z,
         }
     }
+}
+
+pub fn collide(origin: Vector, dir: Vector, center: Vector, r: f64) -> Option<Vector> {
+    let l = center - origin;
+    let tca = l.dot(dir);
+    let d2 = l.dot(l) - tca * tca;
+
+    let r2 = r * r;
+
+    if d2 > r2 {
+        return None;
+    }
+
+    let thc = (r2 - d2).sqrt();
+
+    let mut t0 = tca - thc;
+    let mut t1 = tca + thc;
+
+    if t0 > t1 {
+        std::mem::swap(&mut t0, &mut t1);
+    }
+
+    if t0 < 0.0 {
+        t0 = t1;
+        if t0 < 0.0 {
+            return None;
+        }
+    }
+
+    return Some(origin + dir * t0);
 }
